12913345b3b037b5d8df630903e2c8f917a786cb
Rohit Singh <rohitsingh2020rdd@gmail.com>
2026-02-07 22:26:24 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package Firstlession;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("HelloWorld");
+  }
+  
+}
+/*add two numbers;
+AddTwoNumbers-->pascal naming comvention..(class me use)
+addTwoNumbers-->camel naming convention..(function me use)*/
